150146e753b6b60755fb75354b9e2f93e7cf6ae7
DioPires <diogo.m.pires@tecnico.ulisboa.pt>
2017-12-08 17:23:20 +0000

    Added script used to run all the tests at once

---
+++ run_tests.py
@@ -0,0 +1,7 @@
+import unittest
+from test_stations import TestStations
+from test_25000_words import TestMediumSet
+from test_466000_words import TestBigSet
+
+if __name__ == '__main__':
+    unittest.main()
