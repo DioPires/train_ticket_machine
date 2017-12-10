b4d016fe63f589764f46e67a19005ada8945aa67
DioPires <diogo.m.pires@tecnico.ulisboa.pt>
2017-12-10 01:10:37 +0000

    Moved test files and test data into a new directory

--- tests/test_25000_words.py
+++ tests/test_25000_words.py
@@ -1,20 +1,18 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(".."))
 import unittest
 from trie import Trie
 
-class TestStations(unittest.TestCase):
+class TestMediumSet(unittest.TestCase):
     trie = Trie()
-    words = ["Dartford", "Dartmouth", "Tower Hill",
-             "Derby", "Liverpool", "Liverpool Line Street",
-             "Paddington", "Euston", "London Bridge", "Victoria"]
+    with open("25000_words.txt") as f_hdl:
+        words = [entry.replace("\n", "") for entry in f_hdl.readlines()]
     trie.add_words(words)
 
     def test_prefix(self):
-        expected_result = {'matches': ['Dartford', 'Dartmouth'], 'next_chars': ['m', 'f']}
-        self.assertEqual(self.trie.from_prefix("dart"), expected_result)
-
-    def test_prefix_space(self):
-        expected_result = {'matches': ['Liverpool', 'Liverpool Line Street'], 'next_chars': [' ']}
-        self.assertEqual(self.trie.from_prefix("liverpool"), expected_result)
+        expected_result = {'matches': ['zygote'], 'next_chars': ['o']}
+        self.assertEqual(self.trie.from_prefix("zyg"), expected_result)
 
     def test_wrong_prefix(self):
         expected_result = {'matches': [], 'next_chars': []}
@@ -27,10 +25,10 @@ class TestStations(unittest.TestCase):
         self.assertRaises(ValueError, self.trie.from_prefix, None)
 
     def test_word_exists(self):
-        self.assertTrue(self.trie.word_exists("liverpool line street"))
+        self.assertTrue(self.trie.word_exists("ZYGOTE"))
 
     def test_word_exists_force_case(self):
-        self.assertFalse(self.trie.word_exists("liverpool line street", ignore_case=False))
+        self.assertFalse(self.trie.word_exists("ZYGOTE", ignore_case=False))
 
     def test_nonstr_word_exists(self):
         self.assertRaises(TypeError, self.trie.word_exists, ["zyg", "z"])
--- tests/test_466000_words.py
+++ tests/test_466000_words.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(".."))
 import unittest
 from trie import Trie
 
